ddfcd3024a3e16eca5f8d81115068204f9313cd7
toandoan0301 <toan.doanchi044@gmail.com>
2024-01-11 11:03:14 +0700

    hot fix login

--- src/main/java/com/blueberry/BlueBerryApplication.java
+++ src/main/java/com/blueberry/BlueBerryApplication.java
@@ -2,8 +2,10 @@ package com.blueberry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BlueBerryApplication {
 
 	public static void main(String[] args) {
