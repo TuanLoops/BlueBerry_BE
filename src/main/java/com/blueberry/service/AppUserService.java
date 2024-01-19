8b08bcb3bfb5afdb756a404cc066584118ec44e9
toandoan0301 <toan.doanchi044@gmail.com>
2024-01-19 14:22:40 +0700

    add search friend

--- src/main/java/com/blueberry/service/AppUserService.java
+++ src/main/java/com/blueberry/service/AppUserService.java
@@ -3,6 +3,7 @@ package com.blueberry.service;
 import com.blueberry.model.acc.User;
 import com.blueberry.model.acc.UserPrinciple;
 import com.blueberry.model.app.AppUser;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,4 +12,6 @@ public interface AppUserService extends GenericService<AppUser>{
 
     AppUser findByUserName(String username);
     AppUser getCurrentAppUser();
+
+    Iterable<AppUser> findByName(String name, boolean banned);
 }
