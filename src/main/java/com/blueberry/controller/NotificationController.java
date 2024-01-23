e7599f456d215f35f2e77e411ef80ab9e2b8a286
TranTuanCG <tuantn.work@gmail.com>
2024-01-23 11:20:19 +0700

    commit change

--- src/main/java/com/blueberry/controller/NotificationController.java
+++ src/main/java/com/blueberry/controller/NotificationController.java
@@ -9,8 +9,6 @@ import com.blueberry.util.ModelMapperUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
