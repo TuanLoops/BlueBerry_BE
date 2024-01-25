e0c2d6a77d2a966690173af49586fb7dfb54cf30
Khowataooi <hadesnd1505@gmail.com>
2024-01-25 15:20:02 +0700

    final

--- src/main/java/com/blueberry/service/impl/FirestoreService.java
+++ src/main/java/com/blueberry/service/impl/FirestoreService.java
@@ -3,7 +3,7 @@ package com.blueberry.service.impl;
 import com.blueberry.model.app.ChatRoom;
 import com.blueberry.model.app.Notification;
 import com.blueberry.model.dto.ChatRoomFireStoreDTO;
-import com.blueberry.model.dto.NotificationDTO;
+import com.blueberry.model.dto.NotificationFireStoreDTO;
 import com.blueberry.util.ModelMapperUtil;
 import com.google.cloud.firestore.*;
 import lombok.AllArgsConstructor;
@@ -17,7 +17,7 @@ public class FirestoreService {
     private ModelMapperUtil modelMapperUtil;
 
     public void saveNotification(Notification notification) {
-        NotificationDTO notificationDTO = modelMapperUtil.map(notification, NotificationDTO.class);
+        NotificationFireStoreDTO notificationDTO = modelMapperUtil.map(notification, NotificationFireStoreDTO.class);
 
         firestore.collection("notifications").document(notification.getId())
                 .set(notificationDTO);
